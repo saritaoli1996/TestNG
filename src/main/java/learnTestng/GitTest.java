295164a3919bd43aac6f33944c15b7cf092c07ff
saritaoli1996 <you@example.com>
2021-07-24 17:20:17 -0500

    2nd commit

---
+++ src/main/java/learnTestng/GitTest.java
@@ -0,0 +1,11 @@
+package learnTestng;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		printMe();
+	}
+public static void printMe() {
+	System.out.println("GIT TEST");
+}
+}
